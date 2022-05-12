7e2bb3e23e80bab33e3b0071da7e03fb0dc33390
Pijus <pijus.leonavicius@industrio3d.com>
2022-05-12 23:40:21 +0100

    extraction of interesting pixels. just started

--- 6040udp/IMGinterface.py
+++ 6040udp/IMGinterface.py
@@ -9,10 +9,13 @@ import g
 A module that deals with assembling matrices of captured 
 temperature data, providing them to OpenCV and obtaining data of interest.
 
-Temp range for converting to grayscale 40 - 250 deg (0 to 255) , corresponds to 
-sensor readings 3100 - 5230 (K*10) 
+Temp range for converting to grayscale output values: 
+50 - 100 deg (0 to 255) , corresponds to 
+sensor readings 3230 - 3731 (K*10) 
 '''
-
+CLIP_LOW = 3230
+CLIP_HIGH = 3731
+CLIP_DELTA = CLIP_HIGH - CLIP_LOW
 
 '''
 def frame_builder(pixel_line):
@@ -25,12 +28,26 @@ def frame_builder(pixel_line):
 '''
 def output_stream(frame):
     #print(frame)
-    frame_gray = (255*(frame - numpy.min(frame))/numpy.ptp(frame)).astype('uint8')
-    frame_gray = cv2.flip(frame_gray, 0)
-    ret, thresh = cv2.threshold(frame_gray,70,255,cv2.THRESH_BINARY)
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(frame_gray, contours, -1, (255, 230, 255), 1, cv2.LINE_AA)
-    cv2.imshow('iseethis', cv2.resize(frame_gray,(360,240)))
+    frame = cv2.flip(frame, 0)
+    f_clip = cv2.clip(frame, CLIP_LOW, CLIP_HIGH)
+    f_gray = (255*(f_clip - CLIP_LOW)/CLIP_DELTA).astype('uint8')
+    ret, molten_f = cv2.threshold(frame_gray,254,255,cv2.THRESH_BINARY)
+    ret, solid_f = cv2.threshold(frame_gray,0,255,cv2.THRESH_BINARY) 
+    delta_f = solid_f - molten_f
+    molten_x, molten_y = numpy.nonzero(molten_f)
+    solid_x, solid_y = numpy.nonzero(delta_f)
+    molten_arr = numpy.transpose(numpy.array([frame[molten_x, molten_y], molten_x, molten_y]))
+    #extract values in the same frame column, i.e. having same x value:
+    columns = numpy.unique(molten_arr[:,1])
+    for i in columns:
+         
+        molten[molten[:,1]==i]
+    
+
+    
+    #contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    #cv2.drawContours(frame_gray, contours, -1, (255, 230, 255), 1, cv2.LINE_AA)
+    cv2.imshow('iseethis', cv2.resize(f_gray,(360,240)))
     cv2.waitKey(10)
     #return frame_scaled
     
